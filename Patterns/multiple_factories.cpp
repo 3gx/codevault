b9d5a0ee3348df9b7aeb200c37a9551b151c36c5
evghenii <egaburov@dds.nl>
2015-06-25 15:06:49 +0200

    added knife

--- Patterns/multiple_factories.cpp
+++ Patterns/multiple_factories.cpp
@@ -11,16 +11,16 @@ class WidgetT
   public:
     WidgetT(const std::string& widget_name) : widget_name_{widget_name} 
     {
-      std::cout << "    Widget \"" << widget_name_ << "\" ctor. \n";
+//      std::cout << "    Widget \"" << widget_name_ << "\" ctor. \n";
     }
     ~WidgetT()  
     {
-      std::cout << "    Widget \"" << widget_name_ << "\" dtor. \n";
+//      std::cout << "    Widget \"" << widget_name_ << "\" dtor. \n";
     }
 
-    friend std::unique_ptr<WidgetT> makeSet(const WidgetT& a, const WidgetT& b)
+    friend std::unique_ptr<WidgetT> makeSet(const WidgetT& a, const WidgetT& b, const WidgetT &c)
     {
-      return std::make_unique<WidgetT>("set: "+a.widget_name_+" with "+b.widget_name_);
+      return std::make_unique<WidgetT>("set: "+a.widget_name_+" with "+b.widget_name_+" with "+c.widget_name_);
     }
     std::string name() const { return widget_name_; }
 };
@@ -36,13 +36,13 @@ class Factory
     {
       static bool firstInstance = true;
       if (!firstInstance)
-        throw std::runtime_error(std::string("\n\tFATAL: Instance of a Factory \"")+ factory_name_ + "\" is already in use!\n");
+        throw std::runtime_error(std::string("\n\tFATAL: Instance of a Factory \"")+ factory_name_ + "\"" + ", with an internal unique-id <" + std::to_string(ID) + ">, is already in use!\n");
       firstInstance = false;
-      std::cout << "Factory \"" << factory_name_ << "\" ctor. \n";
+ //     std::cout << "Factory \"" << factory_name_ << "\" ctor. \n";
     }
     ~Factory()  
     {
-      std::cout << "Factory \"" << factory_name_ << "\" dtor. \n";
+  //    std::cout << "Factory \"" << factory_name_ << "\" dtor. \n";
     }
     std::unique_ptr<Widget> makeWidget(const std::string &name) const { return std::make_unique<Widget>(factory_name_+"_"+name); }
     const std::string& factoryName() const { return factory_name_; }
@@ -53,40 +53,39 @@ class Factory
 int main(int argc, char *argv[])
 {
   using namespace std;
+#if 0
   using namespace parse_arguments;
-
   auto nfactory = 1;
   auto params = pack(argc, argv,
       param("number of factories", nfactory, "n","")
       );
-
   cerr << params.parse_all();
+#endif
 
 #if 1
   auto factory1 = Factory<1>("silverware");
   auto factory2 = Factory<2>("bronzeware");
 #else
-  auto factory1 = makeFactory("silverware");
-  auto factory2 = makeFactory("bronzeware");
+  auto factory1 = buildFactory("silverware");
+  auto factory2 = buildFactory("bronzeware");
 #endif
 
-  cerr << " ---- \n";
   auto widget1A = factory1.makeWidget("spoon");
   auto widget1B = factory1.makeWidget("fork");
-  auto set1     = makeSet(*widget1A, *widget1B);
+  auto widget1C = factory1.makeWidget("knife");
+  auto set1     = makeSet(*widget1A, *widget1B, *widget1C);
   cerr << set1->name() << endl;
 
-  cerr << " ---- \n";
  
   auto widget2A = factory2.makeWidget("spoon");
   auto widget2B = factory2.makeWidget("fork");
-  auto set2     = makeSet(*widget2A, *widget2B);
+  auto widget2C = factory2.makeWidget("knife");
+  auto set2     = makeSet(*widget2A, *widget2B, *widget2C);
   cerr << set2->name() << endl;
   
-  cerr << " ---- \n";
  
-#if 0 
-  auto set3 = makeSet(*widget1A, *widget2B);
+#if 0
+  auto set3 = makeSet(*widget1A, *widget1B, *widget2C);
   cerr << set3->name() << endl;
 #endif
 }
