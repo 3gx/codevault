f539e154e390e57d5c10e3deeb7e2c67cdf16b1c
evghenii <egaburov@dds.nl>
2015-06-25 15:46:47 +0200

    added hash

--- Patterns/multiple_factories.cpp
+++ Patterns/multiple_factories.cpp
@@ -29,26 +29,45 @@ class Factory
   private:
     const std::string factory_name_;
     using Widget = WidgetT<ID>;
+
+    enum {ACQUIRE=1, RELEASE=0};
+    void raii(bool acquire)
+    {
+      static std::string original_name = factory_name_;
+      static bool firstInstance = true;
+      if (acquire)
+      {
+        if (!firstInstance)
+          throw std::runtime_error(std::string("\n  FATAL: Instance of a Factory  with an unique-id <" + std::to_string(ID) + "> and name \""+original_name+"\", can't be used with a Factory \""+factory_name_+"\".\n"));
+        original_name = factory_name_;
+        firstInstance = false;
+      }
+      else
+      {
+        firstInstance = true;
+        original_name = "";
+      }
+    }
   public:
     Factory(const std::string& factory_name)  : factory_name_(factory_name)
     {
-      static std::string original_name = factory_name;
-      static bool firstInstance = true;
-      if (!firstInstance)
-        throw std::runtime_error(std::string("\n  FATAL: Instance of a Factory  with an unique-id <" + std::to_string(ID) + "> and name \""+original_name+"\", can't be used with a Factory \""+factory_name+"\".\n"));
-      firstInstance = false;
+      raii(ACQUIRE);
     }
     ~Factory()  
     {
+      raii(RELEASE);
     }
     std::unique_ptr<Widget> makeWidget(const std::string &name) const { return std::make_unique<Widget>(name+"_with_"+factory_name_); }
     const std::string& factoryName() const { return factory_name_; }
 };
 
+constexpr size_t hash2(size_t x) { return       ((x >> 16) ^ x);             }
+constexpr size_t hash1(size_t x) { return hash2(((x >> 16) ^ x) * 0x45d9f3b);}
+constexpr size_t hash (size_t x) { return hash1(((x >> 16) ^ x) * 0x45d9f3b);}
 template<size_t ID>
-static std::unique_ptr<Factory<ID>> createFactoryT(const std::string& name)
+static std::unique_ptr<Factory<hash(ID)>> createFactoryT(const std::string& name)
 {
-  return std::make_unique<Factory<ID>>(name);
+  return std::make_unique<Factory<hash(ID)>>(name);
 }
 #define createFactory(name) createFactoryT<__COUNTER__>(name)
 
@@ -65,8 +84,12 @@ int main(int argc, char *argv[])
 #endif
 
   auto factory1 = createFactory("circles");
-  auto factory2 = createFactory("squares");
+#if 1
+  for (int i = 0; i < 10; i++)
+    auto factory2 = createFactory("squares");
+#endif
 
+  auto factory2 = createFactory("squares");
   auto widget1A = factory1->makeWidget("spoon");
   auto widget1B = factory1->makeWidget("fork");
   auto widget1C = factory1->makeWidget("knife");
