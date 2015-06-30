4c364275426ee09a707457353c97e22fba58777e
Evghenii Gaburov <egaburov@dds.nl>
2015-06-30 10:44:05 +0200

    basic versoin runs. working on tuning

--- Dwarfs/0_Misc/random_pattern/random.cpp
+++ Dwarfs/0_Misc/random_pattern/random.cpp
@@ -24,7 +24,9 @@ struct numa_allocator
   T* allocate(size_t size)
   {
     std::cout << "numa_allocator allocate(" << size << ") : " << size*sizeof(T)/1e6 << "MB \n";
-    auto ptr = malloc(size * sizeof(T));
+    void* ptr;
+    posix_memalign(&ptr, 64, size*sizeof(T));
+
     auto p = static_cast<char*>(ptr);
 #pragma omp parallel for schedule(static)
     for (size_t i = 0; i < sizeof(T)*size; i++)
@@ -58,31 +60,54 @@ void bw_test(int ndata, bool seq)
     y[i] = 0.0;
   }
 
-  auto start = std::chrono::steady_clock::now();
+  auto dt_in_sec= 0.0;
 
   const size_t nloop = 10;
   for (size_t iloop = 0; iloop < nloop; iloop++)
   {
-    cout << "iloop= " << iloop << " / " << nloop << endl;
+    cout << "iloop[" << N << "]= " << iloop << " / " << nloop;
+    const auto start = std::chrono::steady_clock::now();
+    asm("#loopBeg");
 #pragma omp parallel for schedule(static)
     for (int j = 0; j < ndata; j++)
     {
+      if (!seq)
+        for (int k = 0; k < 2; k++)
+        {
+          auto inew = idx[min(j+k,ndata-1)];
+          auto hint = _MM_HINT_T0;
+          auto ptr = (char*)&x[inew-1];
+          for (int kk = 0; kk < sizeof(data_type)*3; kk += 64)
+            _mm_prefetch(ptr+kk, hint);
+          for (int kk = 0; kk < sizeof(data_type); kk += 64)
+            _mm_prefetch(((char*)&y[j+k])+kk,_MM_HINT_T0);
+        }
       auto i = idx[j];
       auto im1 = max(i-1,0);
       auto ip1 = min(i+1,ndata-1);
+      auto const & xm1 = x[im1];
+      auto const & xi  = x[i];
+      auto const & xp1 = x[ip1];
+#pragma simd
       for (size_t k = 0; k < N; k++)
-        y[j].data[k] = x[ip1].data[k] - 2*x[i].data[k] + x[im1].data[k];
+        y[j].data[k] += xp1.data[k] - 2*xi.data[k] + xm1.data[k];
     }
-#pragma omp parallel for schedule(static)
-    for (int i = 0; i < ndata; i++)
-      x[i] = y[i];
+    asm("#loopEnd");
+    const auto end = std::chrono::steady_clock::now();
+    auto dt = 1.0e-6*std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    dt_in_sec += dt;
+    cout << " [ " << dt << " sec]\n";
+#if 0
+    if (!seq)
+      std::random_shuffle(idx.begin(), idx.end());
+#endif
   }
 
   
-  const auto end = std::chrono::steady_clock::now();
-  const auto dt_in_sec= 1.0e-6*std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()/nloop;
 
-  const auto bw = ndata*sizeof(data_type)*4/dt_in_sec/1e9;
+  dt_in_sec *= 1.0/nloop;
+  const auto bw = ndata*(sizeof(data_type)*2+sizeof(int))/dt_in_sec/1e9;
+  cout << "sizeof(data_type)= " << sizeof(data_type) << " B\n";
   cout << "dt_per_loop= " << dt_in_sec << "sec\n";
   cout << "BW= " << bw << "GB/s\n";
 
@@ -120,16 +145,16 @@ int main(int argc, char * argv[])
   using namespace std;
   using namespace parse_arguments;
 
-  auto nelems     = 4;
-  auto ndata      = 1;
+  auto nelems     = 8;
+  auto ndata      = 4;
   auto sequential = false;
-  auto use_double = false;
+  auto use_single = false;
 
   auto params = pack(argc, argv, 
       param("data size in M", ndata, "n", "ndata"),
       param("number of elements", nelems, "e", "nelems"),
       param("use sequential", sequential, "seq",""),
-      param("use double", use_double, "d","double")
+      param("use fp32", use_single, "","fp32")
       );
 
 
@@ -137,7 +162,7 @@ int main(int argc, char * argv[])
 
   ndata *= 1024*1024;
 
-  if (use_double)
+  if (!use_single)
     bw_test<double>(ndata, nelems,sequential);
   else
     bw_test<float>(ndata, nelems,sequential);
