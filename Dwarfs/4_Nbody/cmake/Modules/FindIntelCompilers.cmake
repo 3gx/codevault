6a9e1577b1eb51b2d9ca8cbfba601c8cbc05ea03
evghenii <egaburov@dds.nl>
2015-07-02 13:23:31 +0200

    +1

--- Dwarfs/4_Nbody/cmake/Modules/FindIntelCompilers.cmake
+++ Dwarfs/4_Nbody/cmake/Modules/FindIntelCompilers.cmake
@@ -1,16 +1,19 @@
 
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/Languages")
 enable_language(ICPC OPTIONAL)
+#enable_language(IFORT OPTIONAL)
 
 set(ICPC_FOUND False)
 if (${CMAKE_ICPC_COMPILER} STREQUAL "CMAKE_ICPC_COMPILER-NOTFOUND")
 else()
-  macro(icpc_add_executable icpc_target)
+  function(icpc_add_executable icpc_target)
     add_executable(${icpc_target} ${ARGN})
     foreach(file ${ARGN})
       set_source_files_properties(${file} PROPERTIES LANGUAGE ICPC)
     endforeach()
     set_target_properties(${icpc_target} PROPERTIES LINKER_LANGUAGE ICPC)
-  endmacro()
+  endfunction()
   set(ICPC_FOUND True)
+
+  #  function(icpc_copy
 endif()
