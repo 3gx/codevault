f8ad60213a1a95dc657e7e1a073cbff26c87f8aa
Evghenii Gaburov <egaburov@dds.nl>
2015-08-20 09:11:07 +0200

    added puzzles

---
+++ Puzzles/01_using.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+
+  cout << "Hello, puzzling world!" << endl;
+
+  int cout;
+  cout = 10;
+  std::cout << "cout = " << cout << endl;
+  std::cout << "cout << 5 = " << (cout << 5)  << endl;
+
+  return 0;
+}
