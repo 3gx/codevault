fd860cb86c56a22a486d691fb35d944229884611
Evghenii Gaburov <egaburov@dds.nl>
2015-09-07 12:38:29 +0200

    works

--- Dwarfs/1_STL/weld_vtx.cpp
+++ Dwarfs/1_STL/weld_vtx.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "timer.h"
 #include "parse_arguments.h"
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+using Kernel   = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Delaunay = CGAL::Delaunay_triangulation_2<Kernel>;
+using Point    = Kernel::Point_2;
+
 
 using namespace std;
 
@@ -38,17 +47,51 @@ vector<size_t> weld_vertices(vector<Vertex> input)
   // find unique vertcies and erase redundancies
   auto redundant_begin = unique(vertices.begin(), vertices.end());
   vertices.erase(redundant_begin, vertices.end());
+  cout << "\tUnique vertices: " << vertices.size() << endl;
 
   // find index of each vertex in the list of unique vertices
-  auto result_end = my_find_all_lower_bounds(vertices.begin(), vertices.end(),
+  my_find_all_lower_bounds(vertices.begin(), vertices.end(),
       input.begin(), input.end(),
       indices.begin());
-  indices.erase(result_end, indices.end());
   return indices;
+}
+
+vector<Vertex> generate_triangulation(int n_vertices)
+{
+  vector<Point> points(n_vertices);
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static)
+#endif
+  for (int i = 0; i < n_vertices; ++i)
+  {
+    points[i] = Point(drand48(), drand48());
+  }
+
+  Delaunay triangulation;
+  triangulation.insert(points.begin(), points.end());
+
+  vector<Vertex> vertices;
+  for (auto face = triangulation.finite_faces_begin(); face != triangulation.finite_faces_end(); ++face)
+  {
+    auto triangle = triangulation.triangle(face);
+#if 0
+    cout << "Triangle:\t" << triangle << endl;
+    cout << "Vertex 0:\t" << triangle[0][1] << endl;
+    cout << "Vertex 1:\t" << triangle[1] << endl;
+    cout << "Vertex 2:\t" << triangle[2] << endl;
+    cout << " ------\n";
+#endif
+    vertices.emplace_back(triangle[0][0],triangle[0][1]);
+    vertices.emplace_back(triangle[1][0],triangle[1][1]);
+    vertices.emplace_back(triangle[2][0],triangle[2][1]);
+  }
+
+  return vertices;
 }
 
 int main(int argc, char *argv[])
 {
+  srand48((size_t)main);
   using namespace parse_arguments;
 
   auto n_vertices = 1024;
@@ -56,7 +99,20 @@ int main(int argc, char *argv[])
       param("number of vertices", n_vertices, "n", "")
       );
 
-  cerr << params.parse_all();
+  cout << params.parse_all();
+
+  cout << "Generating triangulation...\n";
+  auto vertices = generate_triangulation(n_vertices);
+ 
+  { 
+    Timer _timer("weld_vertices",Timer::verbose_destructor{});
+    cout << "Welding vertices... \n";
+    cout << "\tNumber of vertices port triangulation: " << vertices.size() << endl;
+    _timer.tbeg();
+    auto indices = weld_vertices(vertices);
+    _timer.tend();
+    cout << "\tNumber of indices: " << indices.size() << endl;
+  }
 
   return 0;
 }
